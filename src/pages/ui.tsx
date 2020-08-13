8e05e4fa458ac5c425314c8192a41d32ea1d04cb
Bruno Araújo <bruno.ar.avl@gmail.com>
2020-08-13 01:52:45 -0300

    melhoria no component UI

--- src/pages/ui.tsx
+++ src/pages/ui.tsx
@@ -1,15 +1,30 @@
 import TextLink from '../UI/atoms/Links/TextLink/TextLink'
+import * as S from '../styles/util'
 
 export default function UI() {
   return (
     <>
-      <h1>Atoms</h1>
-      <TextLink href="https://google.com.br">TextLink</TextLink>
-      <hr />
-      <h1>Molecules</h1>
-      <hr />
-      <h1>Organisms</h1>
-      <hr />
+      <section>
+        <article>
+          <h1>Atoms</h1>
+          <S.spaceBlock>
+            <TextLink href="https://google.com.br">TextLink</TextLink>
+          </S.spaceBlock>
+          <hr />
+        </article>
+      </section>
+      <section>
+        <article>
+          <h1>Molecules</h1>
+          <hr />
+        </article>
+      </section>
+      <section>
+        <article>
+          <h1>Organisms</h1>
+          <hr />
+        </article>
+      </section>
     </>
   )
 }
